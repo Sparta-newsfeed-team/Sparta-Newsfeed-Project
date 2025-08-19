cd9ac74da301cf4bd1d27c9aaf6e107fe3704c92
ckscks7878jj <twodc@naver.com>
2025-08-19 13:36:17 +0900

    rename: SignUpRequest → SignupRequest

--- src/main/java/com/example/newsfeedproject/user/dto/SignupRequest.java
+++ src/main/java/com/example/newsfeedproject/user/dto/SignupRequest.java
@@ -1,4 +1,22 @@
 package com.example.newsfeedproject.user.dto;
 
-public class SignupRequest {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+
+        @NotBlank(message = "유저명은 필수 입력사항입니다.")
+        @Size(max = 20, message = "유저명은 최대 20자까지 입력이 가능합니다.")
+        String name,
+
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        String email,
+
+        @Size(max = 3)
+        int age,
+
+        @NotBlank(message = "비밀번호는 필수 입력사항입니다.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
+        String password
+) {}
