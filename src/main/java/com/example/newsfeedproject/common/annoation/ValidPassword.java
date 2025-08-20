cf51e9430c24a9aea5e3219334451b9d655597f8
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-20 16:50:56 +0900

    fix: Custom Valid의 Target에 ElementType.METHOD 추가

--- src/main/java/com/example/newsfeedproject/common/annoation/ValidPassword.java
+++ src/main/java/com/example/newsfeedproject/common/annoation/ValidPassword.java
@@ -13,7 +13,7 @@ import java.lang.annotation.*;
  */
 @Documented
 @Constraint(validatedBy = PasswordValidator.class)
-@Target({ElementType.FIELD})
+@Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidPassword {
 
