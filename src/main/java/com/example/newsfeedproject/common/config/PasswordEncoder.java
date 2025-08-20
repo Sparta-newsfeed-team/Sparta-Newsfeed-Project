d1d2fa3b80719743ab0cdf9a07b2f9d060a5c76e
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-20 12:10:53 +0900

    refactor: common 패키지 코드 리팩토링

--- src/main/java/com/example/newsfeedproject/common/config/PasswordEncoder.java
+++ src/main/java/com/example/newsfeedproject/common/config/PasswordEncoder.java
@@ -3,14 +3,28 @@ package com.example.newsfeedproject.common.config;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
+/**
+ * 사용자 패스워드 인코더 클래스
+ */
 @Component
 public class PasswordEncoder {
 
+    /**
+     * 사용자 패스워드 인코드
+     * @param rawPassword 인코드할 패스워드
+     */
     public String encode(String rawPassword) {
+
         return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
     }
 
+    /**
+     * rawPassword와 encodedPassword가 일치하는 경우 true, 아니면 false 반환
+     * @param rawPassword 확인하기 위한 패스워드
+     * @param encodedPassword : 암호화된 패스워드
+     */
     public boolean matches(String rawPassword, String encodedPassword) {
+
         BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
     }
