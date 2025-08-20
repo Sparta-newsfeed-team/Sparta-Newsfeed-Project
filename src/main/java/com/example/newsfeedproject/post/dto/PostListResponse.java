cbd720b92bffa9ccdbed501146287a72bd34bf86
nyong0313 <ghdyd6625@naver.com>
2025-08-20 12:07:17 +0900

    refactor: Post Dto Code Convention

--- src/main/java/com/example/newsfeedproject/post/dto/PostListResponse.java
+++ src/main/java/com/example/newsfeedproject/post/dto/PostListResponse.java
@@ -3,6 +3,7 @@ package com.example.newsfeedproject.post.dto;
 import java.util.List;
 
 public record PostListResponse(
+
         List<PostResponse> posts,
         int page,
         int totalPages,
