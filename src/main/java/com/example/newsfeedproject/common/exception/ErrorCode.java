55e7cd89a15cd6b6c2f2e27be6303bd9ab84883a
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-20 16:20:31 +0900

    feat: ErrorCode 추가

---
+++ src/main/java/com/example/newsfeedproject/common/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.example.newsfeedproject.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    USER_NOT_FOUND("AUTH-001", "사용자를 찾지 못했습니다.", HttpStatus.BAD_REQUEST),
+    EMAIL_ALREADY_EXISTS("AUTH-002", "이미 존재하는 이메일입니다.", HttpStatus.BAD_REQUEST),
+
+    PASSWORD_INCORRECT("USR-001", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+    PASSWORD_NOT_AVAILABLE("USR-002", "새 비밀번호는 현재 비밀번호와 달라야 합니다.", HttpStatus.BAD_REQUEST),
+    REQUIRED_UPDATE_INFO("USR-003", "이름 또는 나이 중 하나는 반드시 입력되어야 합니다.", HttpStatus.BAD_REQUEST),
+
+    POST_NOT_FOUND("POST-001", "존재하지 않는 게시물입니다.", HttpStatus.BAD_REQUEST),
+
+    CANNOT_FOLLOW_SELF("FOLL-001", "자기 자신을 팔로우할 수 없습니다.", HttpStatus.BAD_REQUEST),
+    ALREADY_FOLLOWED("FOLL-002", "이미 팔로우 한 유저입니다.", HttpStatus.BAD_REQUEST),
+    IS_NOT_FOLLOWING("FOLL-003", "팔로우 관계가 아닙니다.", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+}
